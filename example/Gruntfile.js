3af23c0ab0843c83e14a2fc9291c08de68b58bce
doug <linderd@iinet.net.au>
2014-01-29 16:30:48 +0800

    Updated grunt file

--- example/Gruntfile.js
+++ example/Gruntfile.js
@@ -1,40 +1,58 @@
 module.exports = function (grunt) {
-      grunt.loadNpmTasks('grunt-typescript');
-          grunt.loadNpmTasks('grunt-contrib-watch');
-              grunt.loadNpmTasks('grunt-contrib-connect');
-                  grunt.loadNpmTasks('grunt-open');
-                   
-                      grunt.initConfig({
-                                pkg: grunt.file.readJSON('package.json'),
-                                connect: {
-                                              server: {
-                                                                options: {
-                                                                                      port: 8080,
-                                            base: './'
-                                        }
-                                                                                     }
-                                                              },
-                                typescript: {
-                                              base: {
-                                                                src: ['lib/**/*.ts'],
-                                                                                               dest: 'js/PixelVisionJSDemos.js',
-                                                                                                               options: {
-                                                                                                                                   module: 'amd',
-                                                                                                                                                       target: 'es5'
-                                                                                                                                                                       }
-                                                                                                                                                                                   }
-                                                                                                                                                                                           },
-                                                                                                                                                                                                   watch: {
-                                                                                                                                                                                                               files: '**/*.ts',
-                                                                                                                                                                                                                                       tasks: ['typescript']
-                                                                                                                                                                                                                                               },
-                                                                                                                                                                                                                                                       open: {
-                                                                                                                                                                                                                                                                   dev: {
-                                                                                                                                                                                                                                                                                   path: 'http://localhost:8080/index.html'
-                                                                                                                                                                                                                                                                                               }
-                                                                                                                                                                                                                                                                                                       }
-                                                                                                                                                                                                                                                                                                           });
-                                                                                                                                                                                                                                                                                                            
-                                                                                                                                                                                                                                                                                                               grunt.registerTask('default', ['connect', 'open', 'watch']);
-                                                                                                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                                                                                               }
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-ts');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    connect: {
+      server: {
+        options: {
+          port: 8089,
+          base: './'
+        }
+      }
+    },
+    ts: {
+      lib: {
+        src: ['../*.ts'],
+        out: 'dsync.js',
+        options: {
+          target: 'es3',
+          sourceMaps: false,
+          declaration: true,
+          removeComments: false
+        }
+      },
+      example: {
+        src: ['*.ts'],
+        out: 'example.js',
+        options: {
+          target: 'es3',
+          sourceMaps: false,
+          declaration: false,
+          removeComments: false
+        }
+      }
+    },
+    watch: {
+      lib: {
+        files: '../*.ts',
+        tasks: ['ts:lib', 'ts:example']
+      },
+      example: {
+        files: ['*.ts', '!*.d.ts'],
+        tasks: ['ts:example']
+      }
+    },
+    open: {
+      dev: {
+        path: 'http://localhost:8080/index.html'
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['ts', 'connect', 'open', 'watch']);
+}
