b5fc8a46a13bc8253fa1d8303f1c7f15db682a49
doug <linderd@iinet.net.au>
2014-02-09 09:49:54 +0800

    moved to p2

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
 # dSync
 
-This is a visual state synchronization library.
+This is a state synchronization library.
+
+## Usage
+
+    <script src="../bin/dsync.js"></script>
+    <div id="content1"></div>
+
+    <script>
+      var sync = new dsync.Sync();
+      var model = { content: 'Hello world' };
+      var display = { out: document.getElementById('content1') };
+
+      // Setup events channel
+      sync.channel('hello')
+
+      // State helpers
+      var stateModel = function(m) { return [m.content] };
+      var applyModel = function(m, d) { d.out.innerHTML = m.content; return true; };
+
+      // When model changes, apply to display
+      sync.channel(sync.channels.hello).add(applyModel, stateModel, model, display);
+
+      // Sync!
+      sync.update();
+    </script>
+
+## About
+
+Working directly with dsync can be awkward sometimes, but it provies the basic low
+level operations required to create an Angular.js style live data binding library.
 
 ## Using grunt
 
--- example/physics.ts
+++ example/physics.ts
@@ -1,5 +1,6 @@
 /// <reference path="../bin/dsync.d.ts"/>
 declare var PIXI:any;
+declare var p2:any;
 module physics {
 
     /* Model state for a block object */
@@ -8,7 +9,7 @@ module physics {
         public alive:boolean; /* Is this block still alive? */
         public geom:number[]; /* x, y, width, height, rotation */
         public age:number;    /* How long have we been alive? */
-        public body:any;      /* Box 2D body */
+        public body:any;      /* Physics body */
     }
 
     /* Display for a block object */
@@ -28,41 +29,83 @@ module physics {
         /* Sync target */
         sync:dsync.Sync;
 
-        /* Worker for animation frames */
-        private _worker:any;
 
         constructor(sync:dsync.Sync) {
             this.stage = new PIXI.Stage(0xffffff);
             this.renderer = PIXI.autoDetectRenderer(500, 500);
             document.body.appendChild(<HTMLElement> this.renderer.view);
-            this._worker = () => { this.animate(); };
             this.sync = sync;
         }
 
-        animate():void {
-            requestAnimationFrame(this._worker);
-            this.sync.update();
+        /* Redraw */
+        public redraw():void {
             this.renderer.render(this.stage);
-        }
-
-        start():void {
-            requestAnimationFrame(this._worker);
         }
     }
 
-    /* Box2D world */
-    export class Box2D {
+    /* P2 world */
+    export class P2 {
+
+        /* The physics world */
+        world:any;
+
+        /* Parent for notifications */
+        parent:Demo;
 
         /* Create a world */
+        constructor(parent:Demo) {
+            this.world = new p2.World();
+
+            // Add a floor
+            var planeShape = new p2.Plane();
+            var planeBody = new p2.Body({ position:[0,1] });
+            planeBody.addShape(planeShape);
+            this.world.addBody(planeBody);
+            this.parent = parent;
+        }
 
         /* Create a body for the given block */
+        public body(block:Block):any {
+            var shape = new p2.Rectangle(block.geom[2] * 2, block.geom[3] * 2);
+            var body = new p2.Body({
+                mass:1,
+                position:[block.geom[0],block.geom[1]],
+                angularVelocity:0
+            });
+            body.addShape(shape);
+            this.world.addBody(body);
+            return body;
+        }
+
+        /* Destroy a body */
+        public destroyBody(body:any):void {
+            this.world.removeBody(body);
+        }
+
+        /* Read geometry from a body */
+        public readGeometry(body:any, geom:number[]):void {
+            geom[0] = body.position[0];
+            geom[1] = body.position[1];
+            geom[4] = body.angle;
+        }
 
         /* Timestep */
+        public step():void {
+            this.world.step(1/60);
+            this.parent.sync.touch('body');
+            this.parent.sync.touch('physics');
+        }
     }
 
 
     /* Various actions on block elements */
     export class Demo {
+
+        /* Worker for animation frames */
+        private _worker:any;
+
+        /* Running? */
+        private _running:boolean = true;
 
         /* The sync we'll use for this */
         public sync:dsync.Sync;
@@ -70,19 +113,42 @@ module physics {
         /* Pixi stage */
         public pixi:Pixi;
 
+        /* Physics model */
+        public physics:P2;
+
         /* Set of blocks */
         public blocks:Block[] = [];
 
         constructor() {
             this.sync = new dsync.Sync();
             this.sync.channel('physics');
+            this.sync.channel('body');
             this.pixi = new Pixi(this.sync);
+            this.physics = new P2(this);
+            this._worker = () => { this.step(); };
+        }
+
+        /* Update the model from geometry */
+        public bodySync(b:any, s:Block, changed:boolean[], dt:number):boolean {
+            s.geom[0] = b.position[0];
+            s.geom[1] = b.position[1];
+            s.geom[4] = b.angle;
+            return s.alive;
+        }
+
+        /* Generate a state record for the body */
+        public bodyState(b:any, s:Block, dt:number):any[] {
+            return [b.position[0], b.position[1], b.angle];
         }
 
         /* Update the sprite to match the model */
         public blockSync(b:Block, s:Sprite, changed:boolean[], dt:number):boolean {
+            if (this.physics) {
+                this.physics.readGeometry(b.body, b.geom);
+                console.log(b.body);
+            }
             s.gc.position.x = b.geom[0] * 50;
-            s.gc.position.y = b.geom[1] * 50;
+            s.gc.position.y = 500 - b.geom[1] * 50;
             s.gc.scale.x = b.geom[2];
             s.gc.scale.y = b.geom[3];
             s.gc.rotation = -b.geom[4];
@@ -115,13 +181,37 @@ module physics {
             rtn.parent = this;
             rtn.age = 0;
             this.blocks.push(rtn);
+            rtn.body = this.physics.body(rtn);
             return rtn;
         }
 
         /* Create a block that is synced to a display object */
         public create(x:number, y:number):void {
-            var chan =  this.sync.channel(this.sync.channels.physics);
-            chan.add<Block, Sprite>(this.blockSync, this.blockState, this._createBlock(x, y), this._createSprite());
+            var block = this._createBlock(x, y);
+            var sprite = this._createSprite();
+
+            var physics =  this.sync.channel('physics');
+            var body =  this.sync.channel('body');
+
+            physics.add<Block, Sprite>(this.blockSync, this.blockState, block, sprite);
+            body.add<any, Block>(this.bodySync, this.bodyState, block.body, block);
+        }
+
+        step():void {
+            if (this._running) {
+                requestAnimationFrame(this._worker);
+                this.physics.step();
+                this.sync.update();
+                this.pixi.redraw();
+            }
+        }
+
+        start():void {
+            requestAnimationFrame(this._worker);
+        }
+
+        stop():void {
+            this._running = false;
         }
     }
 }
